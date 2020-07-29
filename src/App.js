69957e0f2bedd8ecdc6314575474979fc6286e98
Morris Ma <mo396720@gmail.com>
2020-07-29 15:17:03 -0700

    dark theme attempt

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, {Component} from 'react';
+import { useState } from 'react';
 import './App.css';
 import Todos from './components/Todos';
 import Header from './components/layout/Header';
 import AddTodo from './components/AddTodo';
 // import nextId from 'react-id-generator';  
 import axios from 'axios'; 
+import { ThemeProvider } from 'styled-components';
+import { lightTheme, darkTheme } from './components/Theme';
+import { GlobalStyles } from './components/Global';
 
 class App extends Component {
   state = {
     todos: []
   }
 
+  // Connect to BackEnd API using axios 
   componentDidMount() {
       axios
         .get('http://127.0.0.1:8000/api/task-list/')
@@ -51,17 +56,33 @@ class App extends Component {
       .then (res => this.setState({todos: [...this.state.todos, res.data]}));
     
   }
+  
+  themeStuff (){
+    const [theme, setTheme] = useState('light');
+    const toggleTheme = () => {
+      if (theme === 'light') {
+        setTheme('dark');
+      } else {
+        setTheme('light');
+      }
+    }
+  }
 
   render(){
-  return (
-    <div className="App">
-      <div className="container">
-        <Header/>
-        <AddTodo addTodo={this.addTodo}/>
-        <Todos todos={this.state.todos} markComplete={this.markComplete} deleteTodoItem={this.deleteTodoItem}/>
-      </div>
-    </div>
-  );
-}}
+    return (
+      <ThemeProvider theme={darkTheme}>
+        <>
+          <GlobalStyles /> 
+          <div className="App">
+            <div className="container">
+              <Header theme={this.themeStuff.theme} toggleTheme={this.themeStuff.toggleTheme}/>
+              <AddTodo addTodo={this.addTodo}/>
+              <Todos todos={this.state.todos} markComplete={this.markComplete} deleteTodoItem={this.deleteTodoItem}/>
+            </div>
+          </div>
+        </>
+      </ThemeProvider>
+    );
+  }}
 
 export default App;
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -5,17 +5,23 @@ import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import Toolbar from '@material-ui/core/Toolbar';
 import Brightness4OutlinedIcon from '@material-ui/icons/Brightness4Outlined';
+import MenuIcon from '@material-ui/icons/Menu';
+import PropTypes from 'prop-types'; 
+
 
 function Header () {
     return (
         <AppBar style={headerStyle}>
             <Toolbar>
+                <IconButton edge="start"  color="inherit" aria-label="menu">
+                    <MenuIcon style= {{marginRight: "10px"}}/> 
+                </IconButton>
                 <Typography variant="h4" >
                     📝 ToDoz
                 </Typography>
                 <IconButton edge="end"  color="inherit" aria-label="menu">
-                    <Brightness4OutlinedIcon
-                        style= {{textAlign: "end"}}
+                    <Brightness4OutlinedIcon onClick={this.props.theme} 
+                        // style= {{justifyContent: "right"}}
                     />
                 </IconButton>
             </Toolbar>
@@ -26,5 +32,9 @@ function Header () {
 const headerStyle ={
     textAlign: 'left',
 }
+
+// Header.propTypes = {
+//     todo: PropTypes.object.isRequired
+// }
 
 export default Header; 
